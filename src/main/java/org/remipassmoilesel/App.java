f9f65015875dbf1b11b8bc7181eb7a3cb053736d
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-10-19 10:51:45 +0200

    First project setup

---
+++ src/main/java/org/remipassmoilesel/App.java
@@ -0,0 +1,13 @@
+package org.remipassmoilesel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
