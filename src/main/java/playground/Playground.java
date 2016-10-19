1da61e6068724767b5feaf535b7e55e97282031c
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-10-19 12:06:19 +0200

    Add playground

---
+++ src/main/java/playground/Playground.java
@@ -0,0 +1,21 @@
+package playground;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * Created by remipassmoilesel on 19/10/16.
+ */
+public class Playground {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(System.currentTimeMillis());
+
+
+
+    }
+    
+
+
+}
