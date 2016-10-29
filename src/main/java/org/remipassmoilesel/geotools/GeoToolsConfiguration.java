ba0b102555283fe2d445f8ce4816983bdc2e72cb
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-10-29 15:32:59 +0200

    Working on geotools config

---
+++ src/main/java/org/remipassmoilesel/geotools/GeoToolsConfiguration.java
@@ -0,0 +1,20 @@
+package org.remipassmoilesel.geotools;
+
+import org.geotools.factory.GeoTools;
+import org.geotools.factory.Hints;
+import org.geotools.xml.NullEntityResolver;
+
+/**
+ * Example of geotools library config.
+ */
+public class GeoToolsConfiguration {
+
+    public static void main(String[] args) {
+        System.out.println("GeoTools.getVersion()");
+        System.out.println(GeoTools.getVersion());
+
+        Hints.putSystemDefault(Hints.ENTITY_RESOLVER, NullEntityResolver.INSTANCE);
+        Hints.removeSystemDefault(Hints.ENTITY_RESOLVER);
+    }
+
+}
