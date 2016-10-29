250c9aea71eea8eec8a9bda567709a43595b5d4f
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-10-29 15:35:16 +0200

    Project configuration

--- src/main/java/playground/Playground.java
+++ src/main/java/playground/Playground.java
@@ -1,9 +1,26 @@
 package playground;
 
+import org.geotools.coverage.CoverageFactoryFinder;
+import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.feature.DefaultFeatureCollection;
+import org.geotools.geometry.jts.JTSFactoryFinder;
+import org.geotools.map.FeatureLayer;
+import org.geotools.map.Layer;
+import org.geotools.map.MapContent;
+import org.geotools.map.MapViewport;
+import org.geotools.referencing.CRS;
+import org.geotools.referencing.factory.epsg.CartesianAuthorityFactory;
+import org.geotools.styling.SLD;
+import org.geotools.styling.Style;
 import org.geotools.styling.StyleFactory;
 import org.opengis.filter.FilterFactory2;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.crs.CRSAuthorityFactory;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.remipassmoilesel.utils.GuiBuilder;
 
+import java.awt.*;
 import java.text.SimpleDateFormat;
 
 /**
@@ -14,14 +31,10 @@ public class Playground {
     private static StyleFactory sf = CommonFactoryFinder.getStyleFactory();
     private static FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2();
 
-    public static void main(String[] args) {
-
-
-        System.out.println(ff.literal("a"));
+    public static void main(String[] args) throws FactoryException {
 
 
     }
-    
 
 
 }
