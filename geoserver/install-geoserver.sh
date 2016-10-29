fe8f670a30c02bbc9285a89f45c29060329f0d8a
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-10-29 15:32:16 +0200

    Add geoserver

---
+++ geoserver/install-geoserver.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo Downloading ...
+wget http://sourceforge.net/projects/geoserver/files/GeoServer/2.9.2/geoserver-2.9.2-bin.zip -O geoserver.zip
+
+echo Inflate ...
+unzip geoserver.zip
+
+
+
