1c87de27c17baac76789345ed02421ef5aaff9c7
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-10-31 22:58:13 +0100

    Add first custom component

--- src/main/java/org/remipassmoilesel/render/RenderLab.java
+++ src/main/java/org/remipassmoilesel/render/RenderLab.java
@@ -6,67 +6,111 @@ import org.geotools.data.simple.SimpleFeatureSource;
 import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.map.FeatureLayer;
 import org.geotools.map.MapContent;
-import org.geotools.map.MapViewport;
 import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.geotools.renderer.GTRenderer;
 import org.geotools.renderer.RenderListener;
 import org.geotools.renderer.lite.StreamingRenderer;
 import org.geotools.styling.SLD;
 import org.opengis.feature.simple.SimpleFeature;
-import org.opengis.geometry.BoundingBox;
-import org.remipassmoilesel.utils.GuiUtils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Small trials on map rendering for Swing
- *
+ * <p>
  * Goal is to create a custom Swing component, to experiment Geotools rendering
- *
+ * <p>
+ * You can drag the map and reset its position
  */
-public class RenderLab extends JPanel {
+public class RenderLab extends JPanel implements MouseListener, MouseMotionListener {
 
+    private final DefaultGeographicCRS crs;
+    private final ReentrantLock renderLock;
+    private final ReferencedEnvelope originalBoundsToRender;
     private BufferedImage contentImage;
     private Dimension contentDim;
     private ReferencedEnvelope mapBoundsToRender;
+    private Point lastDragPoint;
+    private MapContent mapContent;
 
     public static void main(String[] args) {
 
         SwingUtilities.invokeLater(() -> {
 
+            // frame
             JFrame frame = new JFrame();
             frame.setTitle("First rendering trial");
             frame.setSize(new Dimension(810, 610));
             frame.setLayout(new BorderLayout());
 
-            RenderLab content = new RenderLab();
-            frame.add(content, BorderLayout.CENTER);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            // panel where is displayed map
+            RenderLab mapPanel = new RenderLab();
+            frame.add(mapPanel, BorderLayout.CENTER);
+
+            // button to reset map
+            JButton resetButton = new JButton("Reset");
+            resetButton.addActionListener((actionEvent) -> {
+                mapPanel.resetContent();
+            });
+            frame.add(resetButton, BorderLayout.NORTH);
 
             frame.setVisible(true);
         });
 
     }
 
+
     public RenderLab() throws HeadlessException {
         super();
         setSize(new Dimension(800, 600));
+
+        addMouseMotionListener(this);
+        addMouseListener(this);
+
+        this.crs = DefaultGeographicCRS.WGS84;
+
+        // original layer max bounds: ReferencedEnvelope[-61.80976386945311 : 55.83669235015563, -21.389730741571007 : 51.08984197104998]
+
+        // France metrop
+        // this.mapBoundsToRender = new ReferencedEnvelope(-5.40d, 9.91d, 41d, 51.08d, DefaultGeographicCRS.WGS84);
+
+        // Cotentin
+        this.originalBoundsToRender = new ReferencedEnvelope(-2.90d, 1.32d, 47.60d, 50.41d, this.crs);
+        this.mapBoundsToRender = new ReferencedEnvelope(originalBoundsToRender);
+
+        this.renderLock = new ReentrantLock();
+
+        renderImageInThread();
+
+    }
+
+    private void renderImageInThread() {
         new Thread(() -> {
             try {
-                createImage();
+                renderImage();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }).start();
     }
 
-    private void createImage() throws IOException {
+
+    private void renderImage() throws IOException {
+
+        if (renderLock.tryLock() == false) {
+            return;
+        }
 
         // retrieve a shapefile and add it to a mapcontent
         Path shape = Paths.get("data/france-communes/communes-20160119.shp");
@@ -77,15 +121,15 @@ public class RenderLab extends JPanel {
 
         FeatureLayer layer = new FeatureLayer(shapeFileSource, SLD.createLineStyle(Color.blue, 0.2f));
 
-        MapContent content = new MapContent();
-        content.addLayer(layer);
+        mapContent = new MapContent();
+        mapContent.addLayer(layer);
 
         // for debug purposes
-//        GuiUtils.showInWindow(content);
+        // GuiUtils.showInWindow(content);
 
         // get a renderer instance
         GTRenderer renderer = new StreamingRenderer();
-        renderer.setMapContent(content);
+        renderer.setMapContent(mapContent);
 
         RenderingHints hints = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         renderer.setJava2DHints(hints);
@@ -107,16 +151,9 @@ public class RenderLab extends JPanel {
         this.contentDim = new Dimension(800, 600);
         this.contentImage = new BufferedImage(contentDim.width, contentDim.height, BufferedImage.TYPE_INT_ARGB);
 
-        System.out.println("Max bounds of layer:");
-        // original layer max bounds: ReferencedEnvelope[-61.80976386945311 : 55.83669235015563, -21.389730741571007 : 51.08984197104998]
-        System.out.println(layer.getBounds());
-
-        // France metrop
-        this.mapBoundsToRender = new ReferencedEnvelope(-5.40d, 9.91d, 41d, 51.08d, DefaultGeographicCRS.WGS84);
-
         // preserve ratio, optionnal
-        //MapViewport vp = content.getViewport();
-        //vp.setMatchingAspectRatio(true);
+        // MapViewport vp = content.getViewport();
+        // vp.setMatchingAspectRatio(true);
 
         Graphics2D g2d = contentImage.createGraphics();
         renderer.paint(g2d, new Rectangle(contentDim), mapBoundsToRender);
@@ -125,24 +162,99 @@ public class RenderLab extends JPanel {
 
         System.out.println("End of rendering process");
 
+        renderLock.unlock();
+
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        Graphics2D g2d = (Graphics2D) g;
+
+        // paint background
+        g2d.setColor(new Color(255, 248, 210));
+        g2d.fillRect(0, 0, getWidth(), getHeight());
+
         if (contentImage != null) {
-
-            Graphics2D g2d = (Graphics2D) g;
-
-            // paint background
-            g2d.setColor(new Color(255, 248, 210));
-            g2d.fillRect(0, 0, getWidth(), getHeight());
 
             // paint map
             g2d.drawImage(contentImage, 0, 0, contentDim.width, contentDim.height, null);
 
         }
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+        if (this.lastDragPoint == null) {
+            this.lastDragPoint = e.getPoint();
+            return;
+        }
+
+        Point mpos = e.getPoint();
+        double mx = (lastDragPoint.x - mpos.x) / 100d;
+        double my = (lastDragPoint.y - mpos.y) / 100d;
+
+//        System.out.println();
+//        System.out.println(mx);
+//        System.out.println(my);
+
+        lastDragPoint = mpos;
+
+        double x1 = mapBoundsToRender.getMinX() + mx;
+        double x2 = mapBoundsToRender.getMaxX() + mx;
+        double y1 = mapBoundsToRender.getMinY() - my;
+        double y2 = mapBoundsToRender.getMaxY() - my;
+
+        mapBoundsToRender.setBounds(new ReferencedEnvelope(x1, x2, y1, y2, crs));
+
+//        System.out.println("mapBoundsToRender");
+//        System.out.println(mapBoundsToRender);
+
+        renderImageInThread();
+    }
+
+    /**
+     * Reset display to original bounds
+     */
+    private void resetContent() {
+        this.mapBoundsToRender = new ReferencedEnvelope(originalBoundsToRender);
+        renderImageInThread();
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+        // reset last drag point to avoid drag errors
+        this.lastDragPoint = null;
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
 
     }
 }
